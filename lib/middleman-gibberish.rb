fab3584c38dc6e471c8e46743867ceed6b849da1
ahoward <ara.t.howard@gmail.com>
2014-01-07 09:06:27 -0700

    colorized logging

--- lib/middleman-gibberish.rb
+++ lib/middleman-gibberish.rb
@@ -95,7 +95,7 @@ module ::Middleman
                 generate_page(glob, path, encrypted)
               end
 
-              puts "encrypted #{ path }"
+              info "encrypted #{ path }"
             rescue Object => e
               warn "#{ e.message }(#{ e.class })\n#{ Array(e.backtrace).join(10.chr) }"
               next
@@ -173,6 +173,89 @@ module ::Middleman
       require 'erb'
 
       ::ERB.new(template).result(binding)
+    end
+
+    def log(level, *args, &block)
+      message = args.join(' ')
+
+      if block
+        message << ' ' << block.call.to_s
+      end
+
+      color =
+        case level.to_s
+          when /warn/
+            :yellow
+          when /info/
+            :blue
+          when /error/
+            :red
+          else
+            :white
+        end
+
+      if STDOUT.tty?
+        bleat(message, :color => level)
+      else
+        puts(message)
+      end
+    end
+
+    def warn(*args, &block)
+      log(:warn, *args, &block)
+    end
+
+    def info(*args, &block)
+      log(:info, *args, &block)
+    end
+
+    def bleat(phrase, *args)
+      ansi = {
+        :clear      => "\e[0m",
+        :reset      => "\e[0m",
+        :erase_line => "\e[K",
+        :erase_char => "\e[P",
+        :bold       => "\e[1m",
+        :dark       => "\e[2m",
+        :underline  => "\e[4m",
+        :underscore => "\e[4m",
+        :blink      => "\e[5m",
+        :reverse    => "\e[7m",
+        :concealed  => "\e[8m",
+        :black      => "\e[30m",
+        :red        => "\e[31m",
+        :green      => "\e[32m",
+        :yellow     => "\e[33m",
+        :blue       => "\e[34m",
+        :magenta    => "\e[35m",
+        :cyan       => "\e[36m",
+        :white      => "\e[37m",
+        :on_black   => "\e[40m",
+        :on_red     => "\e[41m",
+        :on_green   => "\e[42m",
+        :on_yellow  => "\e[43m",
+        :on_blue    => "\e[44m",
+        :on_magenta => "\e[45m",
+        :on_cyan    => "\e[46m",
+        :on_white   => "\e[47m"
+      }
+
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      options[:color] = args.shift.to_s.to_sym unless args.empty?
+      keys = options.keys
+      keys.each{|key| options[key.to_s.to_sym] = options.delete(key)}
+
+      color = options[:color]
+      bold = options.has_key?(:bold)
+
+      parts = [phrase]
+      parts.unshift(ansi[color]) if color
+      parts.unshift(ansi[:bold]) if bold
+      parts.push(ansi[:clear]) if parts.size > 1
+
+      method = options[:method] || :puts
+
+      Kernel.send(method, parts.join)
     end
 
     Extensions.register(:gibberish, Gibberish)
